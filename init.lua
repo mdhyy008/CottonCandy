a2b9672642e3db68720f9b4d28e0441a476abce5
大白2017 <1904812793@qq.com>
2017-12-22 16:32:34 +0800

    Create init.lua

---
+++ init.lua
@@ -0,0 +1,21 @@
+appname="彩虹糖"
+appver="2.1"
+appcode="5201314"
+appsdk="21"
+path_pattern=""
+packagename="com.CottonCandy"
+theme="Theme_Material_Light_NoActionBar"
+developer=""
+description=""
+debugmode=false
+user_permission={
+  "CHANGE_NETWORK_STATE",
+  "DOWNLOAD_WITHOUT_NOTIFICATION",
+  "GET_PACKAGE_SIZE",
+  "INTERNET",
+  "READ_EXTERNAL_STORAGE",
+  "READ_PHONE_STATE",
+  "SET_WALLPAPER",
+  "WRITE_EXTERNAL_STORAGE",
+  "WRITE_SETTINGS"
+}
